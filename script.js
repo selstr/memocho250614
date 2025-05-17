3861a52cd9b5233712207bfd6d9055c53199a217
yoonboum <selstr@gmail.com>
2025-05-17 17:48:12 +0900

    PWA 기능 추가

--- script.js
+++ script.js
@@ -120,4 +120,17 @@ function displayTodos() {
         }
         list.appendChild(item);
     });
+}
+
+// Service Worker 등록
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(registration => {
+                console.log('ServiceWorker registration successful');
+            })
+            .catch(err => {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
 } 
