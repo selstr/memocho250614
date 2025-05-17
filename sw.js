0242dd44ecebe2417a737fbf2a6eda37d290c4aa
yoonboum <selstr@gmail.com>
2025-05-17 18:09:55 +0900

    PWA 아이콘 추가

--- sw.js
+++ sw.js
@@ -5,8 +5,7 @@ const urlsToCache = [
     './style.css',
     './script.js',
     './manifest.json',
-    './icon-192x192.png',
-    './icon-512x512.png'
+    './icon.svg'
 ];
 
 self.addEventListener('install', event => {
